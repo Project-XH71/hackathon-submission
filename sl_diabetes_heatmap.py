f9add8757d4b8bf298a3ef2b198c503e48aae717
rehashah9 <111229980+rehashah9@users.noreply.github.com>
2023-05-28 10:44:46 +0530

    Update sl_diabetes_heatmap.py

--- sl_diabetes_heatmap.py
+++ sl_diabetes_heatmap.py
@@ -8,7 +8,7 @@ diabetesData = read_csv('diabetes.csv')
 # to show the plot between glucose, BMI, Age and Outcome
 fig=pyplot.figure(figsize = (15,10))
 sl.header("Diabetes Heatmap")
-sl.write("")
+sl.write("This map displays the Pearson Correlation Coefficient for the corresponding x and y diseases/medical values. This can be used to identify relations between different medical conditions.")
 sns.heatmap (diabetesData.corr(), annot=True, fmt = ".2f", cmap = "cool")
 sl.write(fig)
 pyplot.show()

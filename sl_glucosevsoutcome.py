fef074eb71714fde31101a9bfc964eb2d1e02e78
rehashah9 <111229980+rehashah9@users.noreply.github.com>
2023-05-28 10:49:35 +0530

    Update sl_glucosevsoutcome.py

--- sl_glucosevsoutcome.py
+++ sl_glucosevsoutcome.py
@@ -6,9 +6,9 @@ from pandas import read_csv
 diabetesData = read_csv('diabetes.csv')
 
 # to show the plot between glucose, BMI, Age and Outcome
-fig=pyplot.figure(figsize = (15,10))
-sl.header("Relation of Diabetic condition with BMI, Glucose Level and Age")
+fig=pyplot.rcParams['figure.figsize'] = [45, 30]
+sns.pairplot(data = diabetesData, vars=['Glucose'], hue = 'Outcome')
+sl.header("Relation of Diabetic condition with Glucose Level")
 sl.write("This scatterplot defines the correlation of people having/not having diabetes with their glucose level, BMI and age. From this graph we can clearly see a positive correlation between glucose level and diabetes. Here blue dots represent people who don't have diabetes and the orange dots represent the people who have diabetes.")
-sns.scatterplot(data = diabetesData, x="Glucose", y="BMI", hue="Outcome", size="Age")
 sl.write(fig)
 pyplot.show()

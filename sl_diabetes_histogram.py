97b55727bd89009f0a133a77a6ff3f639620a1a6
rehashah9 <111229980+rehashah9@users.noreply.github.com>
2023-05-28 11:51:56 +0530

    Update sl_diabetes_histogram.py

--- sl_diabetes_histogram.py
+++ sl_diabetes_histogram.py
@@ -1,13 +1,33 @@
-import streamlit as sl
-from matplotlib import pyplot
-from pandas import read_csv
-# Read the data 
-diabetesData = read_csv('diabetes.csv')
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-# To display the histograms
-pyplot.rcParams['figure.figsize'] = [15, 10]
-sl.header("Histograms of Various Attributes in the Diabetes Dataset")
-sl.write("It represents the distribution of every attribute in the diabetes dataset in the form of histograms.")
-fig = diabetesData.hist()
-sl.pyplot(fig)
-pyplot.show()
+def main():
+    st.title("Histogram for Different Attributes")
+    
+    # Load the dataset
+    df = pd.read_csv("diabetes.csv")
+    
+    # Get the numerical columns
+    numeric_columns = df.select_dtypes(include=np.number).columns.tolist()
+    
+    # Create subplots
+    fig, axes = plt.subplots(nrows=len(numeric_columns), ncols=1, figsize=(8, 5 * len(numeric_columns)))
+    
+    # Create histograms for each numerical attribute
+    for i, column in enumerate(numeric_columns):
+        ax = axes[i]
+        ax.hist(df[column], bins="auto")
+        ax.set_xlabel(column)
+        ax.set_ylabel("Frequency")
+        ax.set_title(f"Histogram of {column}")
+    
+    # Adjust layout
+    plt.tight_layout()
+    
+    # Display the histograms using Streamlit
+    st.pyplot(fig)
+
+if __name__ == "__main__":
+    main()
